75c83321cb0b71332f20eb6ea3e449c086363385
Pavel Kogut <kogut.kogut1@mail.ru>
2023-06-05 11:52:17 +0500

    Switching of navigation sections on the account page has been done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('Everything is ready!');
+window.addEventListener('DOMContentLoaded', () => {
+
+
+
+    try {
+        // Переключение навигации на странице account
+        const accountMenuList = document.querySelector('.account-menu ul');
+        const accountMenuItems = accountMenuList.querySelectorAll('li');
+        const sections = document.querySelectorAll('.account__inner section');
+
+        accountMenuList.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target.nodeName === 'LI') {
+                let index = Array.prototype.indexOf.call(target.parentNode.children, target);
+                accountMenuItems.forEach(item => {
+                    item.classList.remove('active');
+                });
+                target.classList.add('active');
+                sections.forEach(item => {
+                    item.classList.add('d-none');
+                });
+                sections[index].classList.remove('d-none');
+            }
+        });
+
+
+        
+    } catch {
+        
+    }
+
+
+});
