b4706841a766e18145be222db6600b2a292db438
Pavel Kogut <kogut.kogut1@mail.ru>
2023-06-30 21:25:17 +0500

    Adaptive of header and burger-menu are finished

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,35 +1,67 @@
-'use strict';
+"use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+
+    // Бургер-меню
+    const toggleMenu = (event) => {
+        let target = event.target;
+        let menuBtn = document.querySelector(".menu-btn");
+        let menu = document.querySelector(".header__mobile-menu");
+
+        const showMenu = () => {
+            menuBtn.classList.toggle("active");
+            menu.classList.toggle("active");
+            document.body.classList.toggle("active");
+        };
+
+        if (target.closest(".menu-btn")) {
+            showMenu();
+        } else if (
+            !target.classList.contains("active") &&
+            menuBtn.classList.contains("active")
+        ) {
+            showMenu();
+        } else if (target.classList.contains("menu__link")) {
+            showMenu(); 
+        } else if (target.classList.contains("body") && menuBtn.classList.contains("active")) {
+            showMenu();
+        }
+    };
+
+    document.addEventListener("click", toggleMenu);
+
+
 
 
 
     try {
         // Переключение навигации на странице account
-        const accountMenuList = document.querySelector('.account-menu ul');
-        const accountMenuItems = accountMenuList.querySelectorAll('li');
-        const sections = document.querySelectorAll('.account__inner section');
+        const accountMenuList = document.querySelector(".account-menu ul");
+        const accountMenuItems = accountMenuList.querySelectorAll("li");
+        const sections = document.querySelectorAll(".account__inner section");
 
-        accountMenuList.addEventListener('click', (event) => {
+        accountMenuList.addEventListener("click", (event) => {
             let target = event.target;
-            if (target.nodeName === 'LI') {
-                let index = Array.prototype.indexOf.call(target.parentNode.children, target);
-                accountMenuItems.forEach(item => {
-                    item.classList.remove('active');
+            if (target.nodeName === "LI") {
+                let index = Array.prototype.indexOf.call(
+                    target.parentNode.children,
+                    target
+                );
+                accountMenuItems.forEach((item) => {
+                    item.classList.remove("active");
                 });
-                target.classList.add('active');
-                sections.forEach(item => {
-                    item.classList.add('d-none');
+                target.classList.add("active");
+                sections.forEach((item) => {
+                    item.classList.add("d-none");
                 });
-                sections[index].classList.remove('d-none');
+                sections[index].classList.remove("d-none");
             }
         });
 
-
-        
     } catch {
-        
+        console.log('Что-то не так');
     }
 
-
+    
+    
 });
